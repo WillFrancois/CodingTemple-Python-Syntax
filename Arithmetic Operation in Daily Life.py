fdd919869611d1104ce8751941bd85899a35d759
WillFrancois <williamjcfrancois@gmail.com>
2024-08-21 18:59:17 -0400

    first commit

---
+++ Arithmetic Operation in Daily Life.py
@@ -0,0 +1,12 @@
+#Task 1
+bread = 5.00
+peanut_butter = 3.50
+jelly = 3.75
+
+total_cost = bread + peanut_butter + jelly
+
+#Task 2
+savings = 10000
+interest = 0.07
+
+new_savings = savings + (savings * interest)
---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,14 @@
+#Task 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2
+response = input("How are you feeling today? ")
+if response == "happy":
+    print("That's great to hear!")
+elif response == "sad":
+    print("I hope your day gets better!")
